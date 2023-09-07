6d4ebca2284f206924042ab06d44b4e519067f88
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-07 15:54:05 -0500

    commit

--- Module1/Hello World/Hello World/Hello_World.py
+++ Module1/Hello World/Hello World/Hello_World.py
@@ -1,3 +1,18 @@
 #setting up repo
 print("Hello, world!")
 print("testing 123")
+#syntax print can only print strings
+A = 100
+
+print("This is a literal string")
+print("Variable A contains an int: " + str(A))
+
+#python is an indented lang
+if(A < 100):
+	print("A is less than " + str(A))
+else:
+	print("A is greater than or equal to " + str(A))
+	
+#Python doesn't have strongly typed variables	
+A = "This is a string now"
+print(A)
