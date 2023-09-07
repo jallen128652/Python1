2c9271bd597989c1739ee8a56de9a66dce336243
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-07 16:22:40 -0500

    commit

--- Module1/Lesson2/Lesson2/Lesson2.py
+++ Module1/Lesson2/Lesson2/Lesson2.py
@@ -1,1 +1,36 @@
+#Variables section
+#you don't declare and initialize variables in python
+#a variable is initialized the moment you assign a value to it
 
+#casting
+from array import typecodes
+
+
+x = str(3)	#x will be '3'
+y = int(3)	#y will be 3
+z = float(3)	#z will be 3.0
+print(x)
+print(y)
+print(z)
+
+#get the type of var
+print(type(x))
+print(type(y))
+print(type(z))
+
+#strings can be contained in '' or ""
+
+#vars are case sensitive
+a = 4
+A = "Sally"
+print(a)
+print(A)
+
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+"""
+#I use camelCase
