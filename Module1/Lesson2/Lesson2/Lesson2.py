d66fa004d0159ae75a8c89781f5814e5c0ff2475
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-08 10:54:54 -0500

    c

--- Module1/Lesson2/Lesson2/Lesson2.py
+++ Module1/Lesson2/Lesson2/Lesson2.py
@@ -126,3 +126,39 @@ print("Python is " + x)
 #**********************End of vars*****************************
 
 #*********************DataTypes Section************************
+#               **python is a class based lang**
+# data types and examples:
+"""
+x = "Hello World"	str	
+x = 20	int	
+x = 20.5	float	
+x = 1j	complex	
+x = ["apple", "banana", "cherry"]	list	
+x = ("apple", "banana", "cherry")	tuple	
+x = range(6)	range	
+x = {"name" : "John", "age" : 36}	dict	
+x = {"apple", "banana", "cherry"}	set	
+x = frozenset({"apple", "banana", "cherry"})	frozenset	
+x = True	bool	
+x = b"Hello"	bytes	
+x = bytearray(5)	bytearray	
+x = memoryview(bytes(5))	memoryview	
+x = None	NoneType
+"""
+# setting specific datatypes and examples
+"""
+x = str("Hello World")	str	
+x = int(20)	int	
+x = float(20.5)	float	
+x = complex(1j)	complex	
+x = list(("apple", "banana", "cherry"))	list	
+x = tuple(("apple", "banana", "cherry"))	tuple	
+x = range(6)	range	
+x = dict(name="John", age=36)	dict	
+x = set(("apple", "banana", "cherry"))	set	
+x = frozenset(("apple", "banana", "cherry"))	frozenset	
+x = bool(5)	bool	
+x = bytes(5)	bytes	
+x = bytearray(5)	bytearray	
+x = memoryview(bytes(5))	memoryview
+"""
