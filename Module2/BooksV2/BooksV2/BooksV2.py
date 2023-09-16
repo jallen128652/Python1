8d89451d1b7d13baa559280fbadca6d2779ae69d
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-16 13:05:33 -0500

    c

--- Module2/BooksV2/BooksV2/BooksV2.py
+++ Module2/BooksV2/BooksV2/BooksV2.py
@@ -1,46 +1,47 @@
 # Author: James Allen 
 # Course: ITSE 1373 7P1
 # Program Name: BooksV2
-# Description: Calculates the total price for books with taxes
-# Note: Remember to save often and commit to git once tested
+# Description: This program provides book inventory data by title or ISBN
 
 #Instructions:
-#Calculate the total price for books with tax
-
-#consts section
-TAX_RATE = 0.0825
-
-#input vars section
-bookName = ""
-bookPrice = 0.0
-numCopies = 0
-
-#calculated vars section
-subTotal = 0.0
-taxes = 0.0
-total = 0.0
+#The program should
+#  loop to ask for multiple books data, entered at the console. After getting the
+#   information for a book, print the following in comma-delimited format. Last
+#    4 characters of ISBN, First 5 letters of Title, Number on hand, price *
+#     on hand with 2 decimal places.
+#vars and consts
+bookData = []
 
 #get user input section
 print("Welcome to the Bookstore! \n\n")
-bookName = input("Enter the name of the book: ")
-# casts the input as a float
-bookPrice = float(input("Enter the price of the book: "))
-# casts the input as a int
-numCopies = int(input("Enter the number of copies you wish to purchase: "))
+books = input("Would you like to add a book title order? (y/n) ")
+# looks until the user enters n
+while books.upper() == "Y":
+	#body of the loop
+	#inputs
+	ISBN = input("\nEnter the 10 digit ISBN: ")
+	title = input("Enter the book title: ")
+	genre = input("Enter the 3 letter book genre code: \n Mystery = MYS, Fantasy = FSY, SciFi = SCI, Non-Fiction = NFI: ")
+	# casts the input as a float
+	price = float(input("Enter the price of the book: $"))
+	paperBack = input("Do you want paperback? (y/n): ")
+	# casts the input as a int
+	onHand = int(input("How many copies are on hand?: "))
 
-#calculations 
-subTotal = bookPrice * numCopies
-taxes = subTotal * TAX_RATE
-total = subTotal + taxes
-
-#output
+	#calculations 
+	qtyPrice = price * onHand	
+	genre = genre.upper()
+	#append the data to list 
+	bookData.append(ISBN[-4:])
+	bookData.append(title[0:5])
+	bookData.append(onHand)
+	bookData.append("Sub total: ${:,.2f}".format(qtyPrice))
+	#output
 	#results separator
-print("________________________________________\n")
-print("Your order details:\n")
-# casts int to str so the concat doesn't throw an error
-print("You selected " + str(numCopies) + " copies of: " + bookName)
-# formats the float vals to 2 decimal precision
-print("Subtotal is: ${:,.2f}".format(subTotal))
-print("Tax is: ${:,.2f}".format(taxes))
-print("Total due is: ${:,.2f}".format(total))
-print("\nThanks for shopping at the Bookstore!")
+	print("________________________________________\n")
+	print("Your order details:\n")
+	#print list in comma delimited format	
+	print(*bookData, sep = ", ")
+	#above = print(bookData[0] + ", " + bookData[1] + ", " + str(bookData[2]) + ", " + str(bookData[3])) 
+	#updates loop control var
+	books = input("Would you like to add another book title order? (y/n) ")
