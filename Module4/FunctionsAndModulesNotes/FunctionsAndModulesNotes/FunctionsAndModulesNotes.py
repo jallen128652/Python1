2c160e6b48bfbcaa7ad53bd6b021260f1d7ecc27
jallen128652 <jallen128652@mymail.tstc.edu>
2023-10-02 10:57:57 -0500

    c

--- Module4/FunctionsAndModulesNotes/FunctionsAndModulesNotes/FunctionsAndModulesNotes.py
+++ Module4/FunctionsAndModulesNotes/FunctionsAndModulesNotes/FunctionsAndModulesNotes.py
@@ -1,1 +1,10 @@
-#*************************Functions Notes*************************
+#*************************Functions and Modules Notes*************************
+"""
+What are functions and modules?
+Function – A block of code that runs when called. You can pass data, and get
+data from functions.
+
+Module – A file of functions you can include in your program
+
+Functions and Modules provide modularity to your program
+"""
