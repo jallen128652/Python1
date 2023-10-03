7fcb699be7e7a7b333386b39bedf18b08bff6dfd
jallen128652 <jallen128652@mymail.tstc.edu>
2023-10-03 15:57:18 -0500

    c

---
+++ Module4/FunctionsAndModulesNotesV5/FunctionsAndModulesNotesV5/FunctionsAndModulesNotesV5.py
@@ -0,0 +1,19 @@
+#*************************Modules Notes*************************
+"""
+What is a Module?
+
+A module is simply a separate python file (.py) that can contain functions and
+data (variables)Modules can be imported into your application.
+
+You can use a module to create code libraries you can use in multiple applications
+
+Adding a Python file
+
+You can use any name for your module file.
+
+The important thing to remember is the module py file has to be in the same
+directory as your python app.
+
+There are ways to import custom modules from directories, but we won't cover them
+in this lesson.
+"""
