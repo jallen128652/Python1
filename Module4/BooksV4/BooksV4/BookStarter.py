5521a20a4146e71df1f7e3f3ed194fce12b8b1cd
jallen128652 <jallen128652@mymail.tstc.edu>
2023-10-04 16:52:17 -0500

    c

--- Module4/BooksV4/BooksV4/BookStarter.py
+++ Module4/BooksV4/BooksV4/BookStarter.py
@@ -22,16 +22,6 @@ def printMenu():
 
 clear = lambda: os.system('cls')
 
-x = printMenu()
-print(x)
-#prompt user for menu option
-# option1 iterate through list with one book per line
-# option2 take inputs and append list and file
-# option3 search the list by book id or title
-# option4 delete a book in entirety( search for book and delete line)
-# option5 write the complete list to output.txt
-# option6 quit (need a loop)
-
 sID = []
 sTitle = []
 sGenre = []
@@ -42,10 +32,10 @@ sAuthorFirst = []
 sAuthorLast = []
 sPublisher = []
 
+printMenu()
+
 file1 = open("book.txt", "r")
-
 for sLine in file1:
-
     record = sLine.strip().split(",")
     sID.append(record[0].strip())
     sTitle.append(record[1].strip())
@@ -56,6 +46,31 @@ for sLine in file1:
     sAuthorFirst.append(record[6].strip())
     sAuthorLast.append(record[7].strip())
     sPublisher.append(record[8].strip())
+
+#prompt user for menu option
+choice = int(input("Enter a menu option number: "))
+while choice != 0:
+    # option1 iterate through list with one book per line
+    if choice == 1: 
+        for x in range(len(sID)):
+            print(str(sID[x]) + ", " + sTitle[x] + ", " + sGenre[x] + ", " +
+                  str(fPrice[x]) + ", " + sPaperback[x] + ", " +
+                 str(iOnHand[x]) + ", " + sAuthorFirst[x] + ", " +
+                sAuthorLast[x] + ", " + sPublisher[x])
+        done = input("\nOnce you complete reviewing the list enter Y to return to the main menu: ")
+        if done.upper() == "Y":
+            printMenu()
+            choice = int(input("Enter a menu option number: "))     
+    # option2 take inputs and append list and file
+    elif choice == 2:
+    # option3 search the list by book id or title
+    elif choice == 3:
+    # option4 delete a book in entirety( search for book and delete line)
+    elif choice == 4:
+    # option5 write the complete list to output.txt
+    elif choice == 5:
+    # option6 quit (need a loop)
+
 
 
 file1.close()
