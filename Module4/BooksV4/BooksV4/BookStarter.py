b52b17c8e2488157016890c1b040c8bcfd016f8d
jallen128652 <jallen128652@mymail.tstc.edu>
2023-10-04 17:37:30 -0500

    c

--- Module4/BooksV4/BooksV4/BookStarter.py
+++ Module4/BooksV4/BooksV4/BookStarter.py
@@ -46,6 +46,8 @@ for sLine in file1:
     sAuthorFirst.append(record[6].strip())
     sAuthorLast.append(record[7].strip())
     sPublisher.append(record[8].strip())
+file1.close()    
+
 
 #prompt user for menu option
 choice = int(input("Enter a menu option number: "))
@@ -63,14 +65,47 @@ while choice != 0:
             choice = int(input("Enter a menu option number: "))     
     # option2 take inputs and append list and file
     elif choice == 2:
+        bID = int(input("Please enter the 4 digit book ID: "))
+        bTitle = input("Please enter the book title: ")
+        bGenre = input("FIC = fiction, PSY = Psychology, SFI = SciFi, MYS = Mystery, HOR = Horror\nCMP = Computers, ART = Art, PHI = Philosophy, POE = Poetry, TRA = Travel\nPlease enter the book genre 3 letter code: ")
+        bPrice = float(input("Please enter the book price: $"))
+        bPaperback = input("Paperback? (Y/N): ")
+        bOnHand = int(input("How many currently on hand? "))
+        aFname = input("Please enter the authors first name: ")
+        aLname = input("Please enter the authors last name: ")
+        bPublisher = input("Please enter the publishers name: ")
+        print("\nYou entered: " + str(bID) + " " + bTitle + " " + 
+              bGenre + " $" + str(bPrice) + " " + bPaperback + " " +
+              str(bOnHand) + " " + aFname + " " + aLname + " " +
+              bPublisher)
+        choice2 = input("\nSave this entry and exit to the menu? (Y/N): ")
+        if choice2.upper() == "Y":
+            sID.append(bID)
+            sTitle.append(bTitle)
+            sGenre.append(bGenre)
+            fPrice.append(bPrice)
+            sPaperback.append(bPaperback.upper())  
+            iOnHand.append(bOnHand)
+            sAuthorFirst.append(aFname)
+            sAuthorLast.append(aLname)
+            sPublisher.append(bPublisher)
+            print("Entry stored.")
+            printMenu()
+            choice = int(input("Enter a menu option number: "))
+        else:
+            printMenu()
+            choice = int(input("Enter a menu option number: "))
+                 
     # option3 search the list by book id or title
     elif choice == 3:
+        print("search for book")
     # option4 delete a book in entirety( search for book and delete line)
     elif choice == 4:
+        print("delete book")
     # option5 write the complete list to output.txt
     elif choice == 5:
+        print("save to file")
     # option6 quit (need a loop)
 
 
 
-file1.close()
