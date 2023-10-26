3ede4b777ba969e23a2afc14be27c2322f272d4f
jallen128652 <jallen128652@mymail.tstc.edu>
2023-10-26 10:17:44 -0500

    c

--- Module5/BooksV5/BooksV5/BookClass.py
+++ Module5/BooksV5/BooksV5/BookClass.py
@@ -1,1 +1,12 @@
-
+class BookClass:
+    def __init__(self, sID, sTitle, sGenre, fPrice, sPaperback, iOnHand, sAuthorFirst, sAuthorLast, sPublisher):
+        self.id = sID
+        self.title = sTitle
+        self.genre = sGenre
+        self.price = fPrice
+        self.paperback = sPaperback
+        self.onHand = iOnHand
+        self.authorFname = sAuthorFirst
+        self.authorLname = sAuthorLast
+        self.publisher = sPublisher
+    def printBookInfo(self):
