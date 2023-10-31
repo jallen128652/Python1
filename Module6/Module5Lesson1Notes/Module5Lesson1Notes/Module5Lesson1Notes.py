93abceb9285b60192cb87a1ba1078ed6c82570e3
jallen128652 <jallen128652@mymail.tstc.edu>
2023-10-31 10:58:48 -0500

    c

--- Module6/Module5Lesson1Notes/Module5Lesson1Notes/Module5Lesson1Notes.py
+++ Module6/Module5Lesson1Notes/Module5Lesson1Notes/Module5Lesson1Notes.py
@@ -49,3 +49,93 @@ The assert keyword is used when debugging code.
 #if condition returns False, AssertionError is raised:
 #assert x > 0, "x should be greater than 0"
 
+"""
+Try and except block:
+The try except block, can be used to handle error that would 
+normally crash a program.
+"""
+#The try block lets you test a block of code for errors.
+
+#The except block lets you handle the error.
+
+#The else block lets you execute code when there is no error.
+
+#The finally block lets you execute code, regardless of the result 
+#of the try- and except blocks.
+
+"""
+Exception Handling
+When an error occurs, or exception as we call it, Python will 
+normally stop and generate an error message.
+
+These exceptions can be handled using the try statement:
+
+Example:
+The try block will generate an exception, because x is not defined:
+"""
+try:
+  print(x)
+except:
+  print("An exception occurred")
+"""
+Since the try block raises an error, the except block will be 
+executed.
+
+Without the try block, the program will crash and raise an error:
+"""
+#Many Exceptions
+#You can define as many exception blocks as you want, e.g. if you 
+#want to execute a special block of code for a special kind of error
+
+#Example
+#Print one message if the try block raises a NameError and another 
+#for other errors:
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
+  
+"""
+Else
+You can use the else keyword to define a block of code to be 
+executed if no errors were raised:
+"""
+#Example
+#In this example, the try block does not generate any error:
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+  
+"""
+Finally
+The finally block, if specified, will be executed regardless if the 
+try block raises an error or not.
+"""
+#Example
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+  
+#This can be useful to close objects and clean up resources:  
+#Example
+#Try to open and write to a file that is not writable:
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
+# the file doesn't exist so it prints the final except statement that's the same 
+# indent as try  
