fc10252ad8094e9c757cba3d96ab94c781dadc2a
jallen128652 <jallen128652@mymail.tstc.edu>
2023-11-01 17:37:29 -0500

    c

--- Module6/Module6ExampleProgram/Module6ExampleProgram/Module6ExampleProgram.py
+++ Module6/Module6ExampleProgram/Module6ExampleProgram/Module6ExampleProgram.py
@@ -1,1 +1,22 @@
 #***********************Example Program*******************
+"""
+Complete Program
+- A common thing programmers do is update data
+- In this program we will be using regex to update incorrect data files
+"""
+import re
+
+file = open("inventory.txt")
+of = open("output.txt")
+
+for readline in file:
+        # begins with 44, has a space and begins with SAN and any char after
+    if re.search("^44\sSAN.", readline) != None:
+        #print(readline) # shows which lines will be changed before you change
+        x = re.sub("^44\sSAN.", "44 SAND", readline)
+        of.write(x) # writes the changed lines to output file
+    else:
+        of.write(readline) # writes the lines to output that don't get changed
+
+file.close()
+of.close()
