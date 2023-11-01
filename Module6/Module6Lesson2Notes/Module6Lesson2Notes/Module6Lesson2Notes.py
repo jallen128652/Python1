829b0faf9ef7b2bfef017d8cc2126f792262e51f
jallen128652 <jallen128652@mymail.tstc.edu>
2023-11-01 11:48:38 -0500

    c

--- Module6/Module6Lesson2Notes/Module6Lesson2Notes/Module6Lesson2Notes.py
+++ Module6/Module6Lesson2Notes/Module6Lesson2Notes/Module6Lesson2Notes.py
@@ -50,3 +50,39 @@ txt = "ID1008, John Doe, Texas"
 #note the . with the * means you can have zero or more occurrences 
 #of any character
 x = re.search("^ID.*Texas$", txt)
+
+"""
+Special Sequences:
+Begin with \
+\A Matches if the specified characters are at the beginning of the 
+string "\AThe"
+
+\b Matches if the specified characters are at the beginning or at 
+the end of a word ("r" string is being treated as a "raw string") 
+r"\bain" r"ain\b"
+
+\B Matches if the specified characters are present, but NOT at the
+beginning (or at the end) of a word ("r" means "raw string") 
+r"\Bain" r"ain\B"
+
+\d Returns a match where the string contains digits (numbers from 
+0-9) "\d"
+
+\D Returns a match where the string DOES NOT contain digits "\D"
+
+\s Returns a match where the string contains a white space 
+character "\s"
+
+\S Returns a match where the string DOES NOT contain a white space 
+character "\S"
+
+\w Returns a match where the string contains any 
+word characters (characters from a to Z, digits from 0-9, and 
+the underscore _ character) "\w"
+
+\W Returns a match where the string DOES NOT contain any word 
+characters "\W"
+
+\Z Returns a match if the specified characters are at the end of 
+the string "Spain\Z"
+"""
