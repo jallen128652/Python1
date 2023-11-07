23119f2a149e0265194f1eebb394eca4429cbb70
jallen128652 <jallen128652@mymail.tstc.edu>
2023-11-07 10:49:04 -0600

    c

---
+++ Module7/Module7Lesson1Notes/Module7Lesson1Notes/Module7Lesson1Notes.py
@@ -0,0 +1,23 @@
+#***********************Tkinter Notes************************
+# example -------------
+"""
+import tkinter
+
+top = tkinter.Tk()
+
+# Code to add widgets will go here...
+
+greeting = tkinter.Label(top, text="CPT Hello")
+greeting.pack()
+
+top.mainloop()
+"""
+
+"""
+Widget Constructor Syntax:
+
+widget = WidgetName(container, **options)
+Container is the parent window or frame the widget goes in.
+Options are arguments for the widgets properties (font, size, color, etc.). Different
+widget types can have different arguments.
+"""
