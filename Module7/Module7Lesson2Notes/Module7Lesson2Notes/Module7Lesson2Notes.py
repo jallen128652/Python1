ccd572bb0d7e4a27e267089d48f840a8f3804230
jallen128652 <jallen128652@mymail.tstc.edu>
2023-11-07 11:39:10 -0600

    c

---
+++ Module7/Module7Lesson2Notes/Module7Lesson2Notes/Module7Lesson2Notes.py
@@ -0,0 +1,13 @@
+#***********************Tkinter Notes part 2*********************
+"""
+Geometry management:
+The pack() Method - This geometry manager organizes widgets in blocks before
+placing them in the parent widget.
+The grid() Method - This geometry manager organizes widgets in a table-like
+structure in the parent widget. Most often used.
+The place() Method - This geometry manager organizes widgets by placing them in a
+specific x,y position in the parent widget.
+There are more options for pack. For sake of time let us look at the other options
+that are more often used. To see more pack options see
+https://docs.python.org/3/library/tkinter.html#packer-options
+"""
